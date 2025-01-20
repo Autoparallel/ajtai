39d330fb4c6c40e6332f7c7bbadf6aff34bf570c
Colin Roberts <colin@primitive.xyz>
2025-01-20 10:07:43 -0700

    Update commitment.rs

--- src/commitment.rs
+++ src/commitment.rs
@@ -4,25 +4,47 @@ use ring::{CyclotomicRing, StandardBasis};
 
 use super::*;
 
+mod sealed {
+  pub trait Sealed {}
+}
+
+pub trait IsHiding: sealed::Sealed {
+  type HidingMatrix;
+  type HidingOpening;
+}
+
+pub struct NonHiding;
+
+pub struct Hiding<R, const M: usize, const V: usize>(PhantomData<R>);
+
+impl<R, const M: usize, const V: usize> sealed::Sealed for Hiding<R, M, V> {}
+impl sealed::Sealed for NonHiding {}
+
+impl<R, const M: usize, const V: usize> IsHiding for Hiding<R, M, V>
+where [(); M + V]:
+{
+  type HidingMatrix = [[R; M]; M + V];
+  type HidingOpening = [R; M + V];
+}
+impl IsHiding for NonHiding {
+  type HidingMatrix = ();
+  type HidingOpening = ();
+}
+
 pub struct CommitmentKey<
   F: PrimeField,
   const D: usize,
   const T: usize,
   const K: usize,
   const M: usize,
-  const B: usize,
+  H: IsHiding,
 > {
-  matrix: [[CyclotomicRing<F, D, T, StandardBasis>; M]; K],
+  matrix:        [[CyclotomicRing<F, D, T, StandardBasis>; M]; K],
+  hiding_matrix: H::HidingMatrix,
 }
 
-impl<
-    F: PrimeField,
-    const D: usize,
-    const T: usize,
-    const K: usize,
-    const M: usize,
-    const B: usize,
-  > CommitmentKey<F, D, T, K, M, B>
+impl<F: PrimeField, const D: usize, const T: usize, const K: usize, const M: usize>
+  CommitmentKey<F, D, T, K, M, NonHiding>
 where
   // D is a power of two
   [(); D.is_power_of_two() as usize - 1]:,
@@ -44,41 +66,48 @@ where
       }
     }
 
-    Self { matrix }
+    Self { matrix, hiding_matrix: () }
   }
 
   // TODO: We can implement a more optimal mat mul?
   pub fn commit(
     &self,
-    val: [CyclotomicRing<F, D, T, StandardBasis>; M],
-  ) -> [CyclotomicRing<F, D, T, StandardBasis>; K] {
-    let mut output = [CyclotomicRing::<F, D, T, StandardBasis>::DEFAULT; K];
+    message: [CyclotomicRing<F, D, T, StandardBasis>; M],
+  ) -> Commitment<F, D, T, K, M, NonHiding> {
+    let mut commitment = [CyclotomicRing::<F, D, T, StandardBasis>::DEFAULT; K];
     for i in 0..K {
       for j in 0..M {
-        output[i] += self.matrix[i][j] * val[j];
+        commitment[i] += self.matrix[i][j] * message[j];
       }
     }
-    output
+    Commitment { commitment, opening_message: message, opening_randomness: () }
   }
 }
 
-pub struct Commitment<F: PrimeField, const D: usize, const T: usize, const K: usize, const B: usize>(
-  [CyclotomicRing<F, D, T, StandardBasis>; K],
-);
+pub struct Commitment<
+  F: PrimeField,
+  const D: usize,
+  const T: usize,
+  const K: usize,
+  const M: usize,
+  H: IsHiding,
+> {
+  commitment:         [CyclotomicRing<F, D, T, StandardBasis>; K],
+  opening_message:    [CyclotomicRing<F, D, T, StandardBasis>; M],
+  opening_randomness: H::HidingOpening,
+}
 
-impl<F: PrimeField, const D: usize, const T: usize, const K: usize, const B: usize>
-  Commitment<F, D, T, K, B>
+impl<F: PrimeField, const D: usize, const T: usize, const K: usize, const M: usize>
+  Commitment<F, D, T, K, M, NonHiding>
 where
   [(); D.is_power_of_two() as usize - 1]:,
   [(); (D % T == 0) as usize - 1]:,
   [(); verify_modulus::<F, T>() as usize - 1]:,
   [(); ((F::NUM_BITS as usize + 7) / 8) * 8]:,
 {
-  pub const fn new(val: [CyclotomicRing<F, D, T, StandardBasis>; K]) -> Commitment<F, D, T, K, B> {
-    Self(val)
+  pub fn sup_norm(&self) -> u64 {
+    self.commitment.iter().map(CyclotomicRing::sup_norm).max().unwrap_or(0)
   }
-
-  pub fn sup_norm(&self) -> u64 { self.0.iter().map(CyclotomicRing::sup_norm).max().unwrap_or(0) }
 }
 
 #[cfg(test)]
@@ -91,7 +120,7 @@ mod tests {
   #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
   fn test_setup() {
     // Create a commitment instance
-    let commitment = CommitmentKey::<MockField, 16, 8, 10, 20, 1>::setup();
+    let commitment = CommitmentKey::<MockField, 16, 8, 10, 20, NonHiding>::setup();
 
     // Check dimensions
     assert_eq!(commitment.matrix[0].len(), 20); // K rows
@@ -113,7 +142,7 @@ mod tests {
     );
 
     // Test that multiple calls generate different matrices
-    let commitment2 = CommitmentKey::<MockField, 16, 8, 10, 20, 1>::setup();
+    let commitment2 = CommitmentKey::<MockField, 16, 8, 10, 20, NonHiding>::setup();
     assert!(
       commitment.matrix != commitment2.matrix,
       "Two random setups produced identical matrices"
@@ -124,11 +153,15 @@ mod tests {
   #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
   fn test_commitment_sup_norm() {
     // Create a commitment with known values
-    let rings = [
+    let commitment = [
       CyclotomicRing::<MockField, 8, 8, StandardBasis>::new([1, 2, 3, 4, 5, 6, 7, 8]),
       CyclotomicRing::<MockField, 8, 8, StandardBasis>::new([0, 16, 15, 14, 13, 12, 11, 10]),
     ];
-    let commitment = Commitment::<MockField, 8, 8, 2, 1>::new(rings);
+    let commitment = Commitment::<MockField, 8, 8, 2, 1, NonHiding> {
+      opening_message: [CyclotomicRing::DEFAULT; 1],
+      commitment,
+      opening_randomness: (),
+    };
 
     // The first ring has max value 8
     // The second ring's values in minimal representation are:
